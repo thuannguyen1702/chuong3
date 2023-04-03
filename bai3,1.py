63ca503f8a22bdf8f509ee2886c915ccf1387ce9
thuannguyen1702 <129154288+thuannguyen1702@users.noreply.github.com>
2023-04-03 11:31:57 +0700

    Add files via upload

---
+++ bai3,1.py
@@ -0,0 +1,9 @@
+a = int(input("nhập số a :"))
+b = int(input("nhập số b :"))
+c = int(input("nhập số c :"))
+max = a
+if b > max : 
+    max = b
+if c > max :
+    max = c
+print("giá trị lớn nhất là :", max)
